2e1abb4160ade37625fd28c9b92d0898c931fa94
Girish Saraf <girishsaraf03@gmail.com>
2024-03-17 17:59:47 -0700

    Modifying main code to accept parameterized arguments

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
-package gostreambridge
+package main
 
 import (
+	"log"
+	"flag"
 	"fmt"
 	"gostreambridge/internal/stream"
 )
 
-func main(){
-	fmt.Printf("Initialized flow")
-	stream.StartStreamBridge("kafka", "mysql")
+func main() {
+	fmt.Printf("Initializing stream bridge")
+
+	// Define flags for two arguments
+	upstreamApp := flag.String("upstreamApp", "", "Provide the upstream app name (kafka / rabbitmq)")
+	downstreamApp := flag.String("downstreamApp", "", "Provide the downstream app name (mysql / elastic / kafka)")
+
+	// Parse command-line arguments
+	flag.Parse()
+
+	// Check if both arguments are provided
+	if *upstreamApp == "" || *downstreamApp == "" {
+		log.Printf("Usage: go run main.go -upstreamApp=valueX -downstreamApp=valueY")
+		return
+	}
+
+	// Convert arguments to map
+	argMap := make(map[string]string)
+	argMap["upstreamApp"] = *upstreamApp
+	argMap["downstreamApp"] = *downstreamApp
+
+
+	stream.StartStreamBridge(argMap)
 }
