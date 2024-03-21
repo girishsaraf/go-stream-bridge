0a2b58eacf4476c43b3d94aee3337aaa6a9573c0
Girish Saraf <girishsaraf03@gmail.com>
2024-03-20 19:16:14 -0700

    Removed struct redundancy

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
-// MySQLConfig Configuration struct for MySQL database
-type MySQLConfig struct {
+// DBConfig Configuration struct for databases
+type DBConfig struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Host     string `json:"host"`
@@ -9,21 +9,33 @@ type MySQLConfig struct {
 	Database string `json:"database"`
 }
 
-// ElasticConfig Configuration struct for Elasticsearch
-type ElasticConfig struct {
-	URL      string `json:"url"` // URL with port
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Index    string `json:"index"`
+// MySQLConfig Configuration struct for MySQL
+type MySQLConfig struct {
+	DBConfig
 }
 
 // SQLServerConfig Configuration struct for SQL Server
 type SQLServerConfig struct {
+	DBConfig
+}
+
+// CommonConfig defines common configuration fields
+type CommonConfig struct {
+	URL      string `json:"url"`
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Host     string `json:"host"`
-	Port     string `json:"port"`
-	Database string `json:"database"`
+}
+
+// ElasticConfig Configuration struct for Elastic
+type ElasticConfig struct {
+	CommonConfig
+	Index string `json:"index"`
+}
+
+// RabbitMQConfig Configuration struct for RabbitMQ
+type RabbitMQConfig struct {
+	CommonConfig
+	Queue string `json:"queue"`
 }
 
 // KafkaConfig Configuration struct for Kafka
@@ -31,12 +43,4 @@ type KafkaConfig struct {
 	Broker  string `json:"brokers"` // Broker with port
 	Topic   string `json:"topic"`
 	GroupId string `json:"groupid"`
-}
-
-// RabbitMQConfig Configuration struct for RabbitMQ
-type RabbitMQConfig struct {
-	URL      string `json:"url"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Queue    string `json:"queue"`
 }
